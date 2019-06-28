88f421c15e9cad8effad4f9be4669f9ee43d586b
ibraheem <ibraheem.fauzi@shipperhq.com>
2019-06-27 21:32:50 -0500

    New code

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 import sqlite3
 
 ROOT = path.dirname(path.relpath((__file__)))
+
+def create_post(clientLocation, companyProfitMargin, SeasonRateFluctuation, CompetitorRate):
+    con = sql.connect(path.join(ROOT, 'database.db'))
+    cur = con.cusor()
+    cur.execute('insert into posts (clientLocation, companyProfitMargin, SeasonRateFluctuation, CompetitorRate) values(?, ?, ?, ?)', (clientLocation, companyProfitMargin, SeasonRateFluctuation, CompetitorRate)
+    con.commit()
+    con.close()
+
+    def get_posts():
+         con = sql.connect(path.join(ROOT, 'database.db'))
+         cur = con.cursor()
+         cur.execute('select * from posts')
+         posts = cur.fetchall()
+         return posts
